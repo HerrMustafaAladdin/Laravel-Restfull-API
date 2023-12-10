6ff2e3d42126abc80e700e300b4f9510da6936e0
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-10 09:56:33 +0330

    fixed ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,26 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    protected function successResponce($code, $data, $message = null)
-    {
-        return response()->json([
-            'status'    =>  'success',
-            'message'   =>  $message,
-            'data'      =>  $data
-        ],$code);
-    }
-
-
-    protected function errorResponce($code, $message)
-    {
-        return response()->json([
-            'status'    =>  'error',
-            'message'   =>  $message,
-            'data'      =>  NULL
-        ],$code);
-    }
+    use ApiResponser;
 }
