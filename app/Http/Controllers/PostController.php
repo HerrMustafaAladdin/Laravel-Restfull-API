ffc50f6b76c05961914075166a44a986121407d4
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-10 11:38:31 +0330

    fixed posr controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends ApiController
 {
@@ -15,6 +14,39 @@ class PostController extends ApiController
     public function index()
     {
         return $this->successResponce(200, Post::all(), 'Get All Data Successfully.');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make(request()->all(),[
+            'title'     =>  'required|string',
+            'body'      =>  'required|string',
+            'image'     =>  'required',
+            'user_id'   =>  'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return $this->errorResponce(422, $validator->messages());
+        }
+
+
+        $post = Post::create([
+            'title'     =>  $request->input('title'),
+            'body'      =>  $request->input('body'),
+            'image'     =>  $request->input('image'),
+            'user_id'   =>  $request->input('user_id'),
+        ]);
+
+        return $this->successResponce(201, $post, 'Create Post Successfully.');
+    }
+
+    public function show(Post $post)
+    {
+        return $this->successResponce(200,$post,"Get [Post : $post->title] successfully .");
     }
 
 }
