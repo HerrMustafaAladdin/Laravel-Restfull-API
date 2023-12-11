e36c27c7c8dbaecc475a6132fec624a5fb3dda3c
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-11 09:11:54 +0330

    added new api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,7 @@ Route::get('/posts',            [PostController::class,'index']);
 Route::post('/posts',           [PostController::class,'store']);
 Route::get('/posts/{post}',     [PostController::class,'show']);
 Route::put('/posts/{post}',     [PostController::class,'update']);
-Route::delete('/posts/{post}',     [PostController::class,'delete']);
+Route::delete('/posts/{post}',  [PostController::class,'delete']);
+
+//=========================================== User
+Route::apiResource('users',UserController::class);
