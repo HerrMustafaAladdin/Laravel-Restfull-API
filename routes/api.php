5fca6ea2e50e0324fe1b746da9b0895990297895
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-11 11:31:01 +0330

    remove old structure

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,16 +14,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-Route::get('/posts',            [PostController::class,'index']);
-Route::post('/posts',           [PostController::class,'store']);
-Route::get('/posts/{post}',     [PostController::class,'show']);
-Route::put('/posts/{post}',     [PostController::class,'update']);
-Route::delete('/posts/{post}',  [PostController::class,'delete']);
-
-//=========================================== User
-Route::apiResource('users',UserController::class);
