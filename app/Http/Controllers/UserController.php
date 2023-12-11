47d20cb3aad70d28c969e4adcc9ba74d7e7a7970
Mostafa Alaeddin <mostafaalaeddin570@gmail.com>
2023-12-11 09:12:06 +0330

    fixed user api controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    use ApiResponser;
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(User::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'      =>  'required|min:3|max:130|string',
+            'email'     =>  'required|email',
+            'password'  =>  'required|min:8|max:120'
+        ]);
+
+        if($validator->fails())
+        {
+            return $this->errorResponce(422,$validator->messages());
+        }
+
+        $user = User::create([
+            'name'      =>  $request->input('name'),
+            'email'     =>  $request->input('email'),
+            'password'  =>  Hash::make($request->input('password'))
+        ]);
+
+        return $this->successResponce(200,$user, 'Create new user successfully.');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return $this->successResponce(200, $user, 'Get User Successfully.');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'      =>  'required|min:3|max:130|string',
+            'email'     =>  'required|email',
+            'password'  =>  'required|min:8|max:120'
+        ]);
+
+        if($validator->fails())
+        {
+            return $this->errorResponce(422, $validator->messages());
+        }
+
+        $user->update([
+            'name'      =>  $request->input('name'),
+            'email'     =>  $request->input('email'),
+            'password'  =>  Hash::make($request->input('password'))
+        ]);
+
+        return $this->successResponce(200,$user, 'Update user successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return $this->successResponce(200, $user, 'Delete User Successfully.');
+    }
+}
